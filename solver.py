75829ad80c5440e377581e6d225c9b681d8c2f3d
Akbar Khan <akbar.khan@enine.school>
2023-12-13 14:58:59 +0530

    Updated the code

--- solver.py
+++ solver.py
@@ -1,8 +1,6 @@
 from math import sqrt, ceil
-import time
 
 def solver(value):
-    start = time.perf_counter()
 
     factors = []
     max_factor = 0
@@ -11,6 +9,9 @@ def solver(value):
         if value%i==0:
             factors.append(i)
             factors.append(value//i)
+
+    if len(factors)==0:
+        return value
 
     for factor in factors:
         flag = 0
@@ -21,8 +22,4 @@ def solver(value):
         
         if flag==0:
             max_factor = factor
-
-    end = time.perf_counter()
-
-    print("This func takes:", (end-start))
     return max_factor
