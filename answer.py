b11064cb31126afedc3f7f5073dc36a16ed25144
Akbar Khan <akbar.khan@enine.school>
2023-12-15 14:18:58 +0530

    Formatted

--- answer.py
+++ answer.py
@@ -1,7 +1,16 @@
+"""
+    Module name :- answer
+    Method(s) :- answer()
+"""
+
 from solver import solver
 
 def answer():
+    """
+    Find the largest prime factor of 600851475143.
+    """
     return solver(600851475143)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(answer())
--- solver.py
+++ solver.py
@@ -1,28 +1,42 @@
+"""
+    Module name :- solver
+    Method(s) :- solver()
+"""
+
 from math import sqrt, ceil
 
 def solver(value):
+    """
+    Find the largest prime factor of a given number.
 
+    Args:
+        value(int) :- Number whose largest prime factor to be found.
+
+    Return:
+        Return the largest prime factor of a given number.
+    """
     factors = []
     max_factor = 0
 
-    for i in range(2,ceil(sqrt(value))):
-        if value%i==0:
+    for i in range(2, ceil(sqrt(value))):
+        if value % i == 0:
             factors.append(i)
-            factors.append(value//i)
+            factors.append(value // i)
 
-    if len(factors)==0:
+    if len(factors) == 0:
         return value
 
     for factor in factors:
         flag = 0
-        for i in range(2,ceil(sqrt(factor))):
-            if factor%i==0:
+        for i in range(2, ceil(sqrt(factor))):
+            if factor % i == 0:
                 flag = 1
                 break
-        
-        if flag==0:
+
+        if flag == 0:
             max_factor = factor
     return max_factor
 
-if __name__ == '__main__':
-    print(f'solver(value=851239123) = {solver(value=851239123)}')
+
+if __name__ == "__main__":
+    print(f"solver(value=851239123) = {solver(value=851239123)}")
